317e50a4a48da332274aa7f98702c3e26c3eb2e0
Vassily Krasnov <mykrasnov@gmail.com>
2019-09-29 16:06:47 +0300

    Add raw html

--- src/app.js
+++ src/app.js
@@ -80,3 +80,11 @@ new Vue({
   el: '#app-8',
   data: obj,
 });
+
+
+const app9 = new Vue({
+  el: '#app-9',
+  data: {
+    rawHtml: '<span style="color:red">Текст должен быть красным</span>'
+  }
+});
