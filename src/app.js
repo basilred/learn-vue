3e13f91fed598a38addf35df4ed3b45f6e0824b3
Vassily Krasnov <mykrasnov@gmail.com>
2019-10-02 17:40:20 +0300

    Add computed property setter

--- src/app.js
+++ src/app.js
@@ -121,8 +121,17 @@ const demo = new Vue({
     lastName: 'Bar',
   },
   computed: {
-    fullName: function() {
-      return this.firstName + ' ' + this.lastName;
+    fullName: {
+      // геттер
+      get: function() {
+        return this.firstName + ' ' + this.lastName;
+      },
+
+      set: function(newValue) {
+        var names = newValue.split(' ');
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1];
+      }
     }
   }
 });
