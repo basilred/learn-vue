0d30fae0f92a8298c2a2ccb02c98463e2cac66b2
Vassily Krasnov <mykrasnov@gmail.com>
2019-10-03 22:11:01 +0300

    Add watcher

--- src/app.js
+++ src/app.js
@@ -135,3 +135,45 @@ const demo = new Vue({
     }
   }
 });
+
+
+const watchExampleVM = new Vue({
+  el: '#watch-example',
+  data: {
+    question: '',
+    answer: 'Пока вы не зададите вопрос, я не могу ответить!',
+  },
+  watch: {
+    // эта функция запускается при любом изменении вопроса
+    question: function (newQuestion, oldQuestion) {
+      this.answer = 'Ожидаю, когда вы закончите печатать...';
+      this.debouncedGetAnswer();
+    }
+  },
+  created: function() {
+    // _.debounce — это функция lodash, позволяющая ограничить то,
+    // насколько часто может выполняться определённая операция.
+    // В данном случае мы ограничиваем частоту обращений к yesno.wtf/api,
+    // дожидаясь завершения печати вопроса перед отправкой ajax-запроса.
+    // Узнать больше о функции _.debounce (и её родственнице _.throttle),
+    // можно в документации: https://lodash.com/docs#debounce
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500);
+  },
+  methods: {
+    getAnswer: function() {
+      if (this.question.indexOf('?') === -1) {
+        this.answer = 'Вопросы обычно заканчиваются вопросительным знаком. ;-)';
+        return;
+      }
+      this.answer = 'Думаю…';
+      const vm = this;
+      axios.get('https://yesno.wtf/api')
+        .then(function(response) {
+          vm.answer = _.capitalize(response.data.answer);
+        })
+        .catch(function(error) {
+          vm.answer = 'Ошибка! Не могу связаться с API. ' + error;
+        });
+    }
+  }
+});
